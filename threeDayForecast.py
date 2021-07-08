3c18c08511985a47f32f5ffc91ab593e1dcc6ba4
Lo <ryan.lo@fairfield.edu>
2021-07-07 23:46:33 -0400

    peak MWh, line chart

--- threeDayForecast.py
+++ threeDayForecast.py
@@ -6,6 +6,7 @@ import numpy as np
 import datetime
 from IPython.display import HTML
 import doMail
+import matplotlib.pyplot as plt
 
 
 
@@ -16,7 +17,7 @@ def retrieveData():
         'info.rmsolutionss@gmail.com', 'JeffKramer1'))
     json_data = response.json()
     demandData = json_data[0]['data']
-    
+
     return demandData
 
 
@@ -31,6 +32,7 @@ def parseData(data):
             row = current[index]['Mw']
             mwData[i].append(row)
         i+=1
+    getPeakData(mwData)
     return mwData
 
 def formatData(data):
@@ -44,21 +46,32 @@ def formatData(data):
         day3.strftime("%m/%d/%y")
         ]
 
+
     df = pd.DataFrame(
         data, 
         columns=[ 
-            'Hour 01', 'Hour 02', 'Hour 03', 'Hour 04', 'Hour 05', 'Hour 06', 
-            'Hour 07', 'Hour 08', 'Hour 09', 'Hour 10', 'Hour 11', 'Hour 12', 
-            'Hour 13', 'Hour 14', 'Hour 15', 'Hour 16', 'Hour 17', 'Hour 18', 
-            'Hour 19', 'Hour 20', 'Hour 21', 'Hour 22', 'Hour 23', 'Hour 24'
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
+            # 'Hour 01', 'Hour 02', 'Hour 03', 'Hour 04', 'Hour 05', 'Hour 06', 
+            # 'Hour 07', 'Hour 08', 'Hour 09', 'Hour 10', 'Hour 11', 'Hour 12', 
+            # 'Hour 13', 'Hour 14', 'Hour 15', 'Hour 16', 'Hour 17', 'Hour 18', 
+            # 'Hour 19', 'Hour 20', 'Hour 21', 'Hour 22', 'Hour 23', 'Hour 24'
             ],
         index= days
     )
     df = df.transpose()
-    return df
     
+    df.plot.line()
+    plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24])
+    plt.xlabel("Hour End")
+    plt.ylabel("MWh")
+    plt.title("Three Day Forecast")
+    plt.show()
+    return df
 
+def getPeakData(data):
+    peak1, peak2, peak3 = max(data[0]), max(data[1]), max(data[2])
 
+    return peak1, peak2, peak3
 
 
 
@@ -66,5 +79,5 @@ if __name__ == "__main__":
     data = retrieveData()
     data = parseData(data)
     df = formatData(data)
-    body = df.to_html()
-    doMail.send_mail(body)
+    # body = df.to_html()
+    # doMail.send_mail(body)
