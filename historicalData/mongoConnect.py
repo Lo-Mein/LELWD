1701fa35138a4941055d372f2345b4160f3b30e7
matthewpagacik12 <64044053+matthewpagacik12@users.noreply.github.com>
2021-07-21 22:58:59 -0400

    changes

--- historicalData/mongoConnect.py
+++ historicalData/mongoConnect.py
@@ -1,9 +1,33 @@
 import pymongo
+from flask import Flask
+from flask_cors import CORS
 
-try:
-    client = pymongo.MongoClinet(
-        "mongodb+srv://RyanMatt:<password>@historicaldata.2fqkr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    )
-    print("connection established")
-except:
-    print("error")
+
+connection_url = "mongodb+srv://RyanMatt:JeffKramer1@historicaldata.2fqkr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+app = Flask(__name__)
+client = pymongo.MongoClient(connection_url)
+
+# Database
+Database = client.get_database("HistoricalData")
+
+# Table
+SampleTable = Database.SampleTable
+
+# To insert a single document into the database,
+# insert_one() function is used
+@app.route("/insert-one/<name>/<id>/", methods=["GET"])
+def insertOne(name, id):
+    queryObject = {"Name": name, "ID": id}
+    query = SampleTable.insert_one(queryObject)
+    return "Query inserted...!!!"
+
+
+def get2015Data():
+    # data = db.2015_hourly_demand
+    demand_list = data.find()
+    for item in demand_list:
+        print(item)
+
+
+# if __name__ == "__main__":
+# data = get2015Data()
